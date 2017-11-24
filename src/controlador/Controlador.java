9222009e199b11440872a58d659aef4c572bf201
Edilson Cichon <edilsoncichon@hotmail.com>
2017-11-24 11:29:40 -0200

    Implementando lógica do calculo.

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,10 +1,30 @@
 package controlador;
 
+import java.util.ArrayList;
+
 public class Controlador {
 
-    public static void main(String[] args) {
+    public Controlador() {
+    }
+
+    public float calcularLucro(){
+        return 0;
         
+             
+        //Custo Total = cf + (cvu*q)
+        //Receita = (P * Q);
+        //Preço = Somatorio(cvu*qt)
+        //Lucro = Receita - Custo Total
         
+    }
+    
+    public float calcularCustosVariaveis(ArrayList<Float> custosVariaveis, ArrayList<Integer> custosMateriaPrima){
+        return 0;
+        
+    }
+    
+    public float calcularCustosFixos(ArrayList<Float> custosFixos){
+        return 0;
         
     }
     
